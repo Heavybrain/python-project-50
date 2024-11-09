2e4158b69caf7979c6d0029a4dfa9dffd1b53476
Heavybrain <mrsaanisimov@mail.ru>
2024-11-09 20:08:47 +0300

    complete 1 stage

--- diff/scripts/gendiff.py
+++ diff/scripts/gendiff.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 import argparse
+import sys
 
 def main():
+    print("Welcome to the Gendiff!")
+    name = input('May I have your name? ')
+    print(f"Hello, {name}")
+
+
     parser = argparse.ArgumentParser(
-        prog='ProgramName',
+        prog='gendiff',
         usage= 'gendiff [-h] first_file second_file',
         description='Compares two configuration files and shows a difference.',
         add_help= True
@@ -11,7 +17,9 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
 
-    parser.print_help()
+    if len(sys.argv) > 1:
+        args = parser.parse_args()
+        parser.print_help()
 
 if __name__ == '__main__':
     main()
